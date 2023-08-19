7367a6bab22b81ecd14e9a4f7f391784b5eb8aa2
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-08-19 20:18:57 +0200

    minor changes

--- apart_eval_hackathon/drop_least_to_most.py
+++ apart_eval_hackathon/drop_least_to_most.py
@@ -1,4 +1,5 @@
 import os
+from typing import cast
 from dotenv import load_dotenv
 import pandas as pd
 
@@ -8,13 +9,36 @@ import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-drop_number_samples = pd.read_csv("drop_number_samples.csv")
-drop_span_samples = pd.read_csv("drop_span_samples.csv")
+drop_number_samples = pd.read_csv("data/drop_number_not_football_samples.csv")
+drop_span_samples = pd.read_csv("data/drop_span_not_football_samples.csv")
+with open("data/drop_ltm_decomposition.txt", "r", encoding="utf-8") as f:
+    prompt_decomposition = f.read()
+with open("data/drop_ltm_solving.txt", "r", encoding="utf-8") as f:
+    prompt_solving = f.read()
+
+Target = float | str
+
+
+def generate_prompt(row: pd.Series) -> tuple[str, Target]:
+    prompt = f"{prompt_decomposition}\n\n{prompt_solving}\n\n"
+    prompt += f"Q: {row['passage']} {row['question']}\nA:"
+    target = cast(float, row["number"]) if "number" in row else cast(str, row["span"])
+    return prompt, target
 
 
 def main() -> None:
-    print(drop_number_samples)
-    print(drop_span_samples)
+    # print(len(drop_number_samples))
+    # print(len(drop_span_samples))
+    # print(len(prompt_decomposition))
+    # print(len(prompt_solving))
+    prompt, target = generate_prompt(drop_number_samples.iloc[0])
+    print(prompt)
+    print(target)
+    print(60 * "=")
+
+    prompt, target = generate_prompt(drop_span_samples.iloc[0])
+    print(prompt)
+    print(target)
 
 
 if __name__ == "__main__":
