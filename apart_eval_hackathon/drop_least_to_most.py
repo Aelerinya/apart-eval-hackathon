a4d8738eba775f81dc573101657cffc3e257fa55
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-08-19 19:43:27 +0200

    separated DROP samples

--- apart_eval_hackathon/drop_least_to_most.py
+++ apart_eval_hackathon/drop_least_to_most.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
@@ -7,11 +8,14 @@ import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-
+drop_number_samples = pd.read_csv("drop_number_samples.csv")
+drop_span_samples = pd.read_csv("drop_span_samples.csv")
 
 
 def main() -> None:
-    ...
+    print(drop_number_samples)
+    print(drop_span_samples)
+
 
 if __name__ == "__main__":
     main()
